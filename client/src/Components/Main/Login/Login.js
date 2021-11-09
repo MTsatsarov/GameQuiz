a788ab5f1570b8940b51b0b00104c8a33bc18b36
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2021-11-09 18:18:41 +0200

    Login page created.

--- client/src/Components/Main/Login/Login.js
+++ client/src/Components/Main/Login/Login.js
@@ -1,7 +1,35 @@
-function Login() {
-    return(
-        <h2>Login Page </h2>
-    )
+import { Component } from "react/cjs/react.production.min"
+import "./Login.css"
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: ''
+        }
+    }
+    LoginSubmitHandler = (e) => {
+        this.setState({ errors: '' })
+        e.preventDefault();
+        var userName = e.target.userName.value;
+        var password = e.target.password.value;
+        if (userName == '' || password == '') {
+            this.setState({ error: 'All fields must be filled' })
+        }
+    }
+
+    render() {
+        return (
+            <div className='login-wrapper' >
+                <h3>Login</h3>
+                <form onSubmit={this.LoginSubmitHandler} >
+                    <span className='validation-errors'>{this.state.error}</span>
+                    <input type='text' name='userName' placeholder='Enter your username ...' />
+                    <input type='password' name="password" placeholder='Enter your password ...' />
+                    <button>Login</button>
+                </form>
+            </div >
+        )
+    }
 }
 
 export default Login
