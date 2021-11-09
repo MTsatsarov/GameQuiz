60cb9aaf82b265e34a709d9f914e386ca34fdd20
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2021-11-09 17:48:42 +0200

    Register form created.

--- client/src/Components/Main/Register/Register.js
+++ client/src/Components/Main/Register/Register.js
@@ -1,7 +1,39 @@
-function Register () {
-    return (
-        <h2>Register Page</h2>
-    )
+import { Component } from "react";
+import "./Register.css"
+
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            error: ''
+        }
+    }
+    onSubmitHandler = (e) => {
+        e.preventDefault();
+        this.setState({error: ''})
+        if (e.target.userName.value == '' || e.target.email.value == '' || e.target.password.value == '' || e.target.rePass.value == '') {
+          this.setState({error:'All fields must be filled'})
+        }
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmitHandler} className='register-form'>
+                <span className='validation-errors'>{this.state.error}</span>
+                <label htmlFor='userName' >Username</label>
+                <input type='text' name='userName' />
+                <label htmlFor='email'>Email</label>
+                <input type='email' name='email' />
+                <label htmlFor='password'>Password</label>
+                <input type='password' name='password' />
+                <label htmlFor='rePass'>Repeat Password</label>
+                <input type='password' name='rePass' />
+                <button type='submit'>Register</button>
+            </form>
+
+        )
+    }
 }
 
 export default Register
