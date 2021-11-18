ae52bfd56e089054955abc0ce948b6f4fea058b1
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2021-11-18 17:56:37 +0200

    Edit form styled.

--- client/src/Components/Main/Edit/EditQuestion/EditQuestion.js
+++ client/src/Components/Main/Edit/EditQuestion/EditQuestion.js
@@ -1,14 +1,25 @@
+import './EditQuestion.css'
 function  EditQuestion(props) {
-    console.log(props);
-    return (
-       <>
-       <input value = {props.info[0].questionName}/>
-       <input value = {props.info[0].firstAnswer}/>
-       <input value = {props.info[0].secondAnswer}/>
-       <input value = {props.info[0].thirdAnswer}/>
-       <input value = {props.info[0].fourthAnswer}/>
-       <input value = {props.info[0].correctIndex}/>
-       </>
+return (
+       <div className='questionsForm'>
+           <label htmlFor='name'>Question Name</label>
+       <input type='text' name='name' value = {props.info[0].questionName}/>
+
+       <label htmlFor='First Answer'>First Answer</label>
+       <input type='text' name='First Answer' value = {props.info[0].firstAnswer}/>
+
+       <label htmlFor='Second Answer'>Second Answer</label>
+       <input type='text'  name='Second Answer' value = {props.info[0].secondAnswer}/>
+
+       <label htmlFor='Third Answer'>Third Answer</label>
+       <input type='text' name='Third Answer'  value = {props.info[0].thirdAnswer}/>
+
+       <label htmlFor='Fourth Answer'>Fourth Answer</label>
+       <input type='text'  name='Fourth Answer' value = {props.info[0].fourthAnswer}/>
+
+       <label htmlFor='Correct Index'>Correct Answer Index</label>
+       <input type='number'  name='Correct Index' value = {props.info[0].correctIndex}/>
+       </div>
     )
 }
 
--- client/src/Components/Main/Edit/EditQuiz/EditQuiz.js
+++ client/src/Components/Main/Edit/EditQuiz/EditQuiz.js
@@ -1,14 +1,16 @@
 import EditQuestion from "../EditQuestion/EditQuestion"
+import "./EditQuiz.css"
 
 function EditQuiz(props) {
-    console.log(props.questionsArray);
     return (
-        <section >
-            <form>
+
+        <section className='editSection ' >
+            <h2>Edit {props.name}</h2>
+            <form className='editForm'>
                 <label className='quiz-name-label' htmlFor='QuizName'>Quiz name</label>
                 <input className='name-input' id='QuizName' type='text' value={props.name} ></input>
-                <div id="dynamicInput">
-                    {props.questionsArray.map((q, i) => <EditQuestion key={i} info={q}  />)}
+                <div id="editQuestion">
+                    {props.questionsArray.map((q, i) => <EditQuestion key={i} info={q} />)}
                 </div>
                 {/* <span className='form-buttons'>
                         <button id='addQuestion' type='button' onClick={this.createQuestion.bind(this)}>Add Question</button>
