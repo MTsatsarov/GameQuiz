96b808f07d2b04757c9739994dcb9001d4bca5d0
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2021-11-23 17:23:55 +0200

    Pagination Done

--- client/src/Components/Main/All/All.js
+++ client/src/Components/Main/All/All.js
@@ -10,22 +10,30 @@ class All extends Component {
         this.state = {
             quizzes: [],
             id: this.props.match.params.id,
-            paginationModel:{}
+            paginationModel: {}
         }
     }
+
+    async getEvents(id) {
+        var allQuizzes = await quiz.GetAll(id);
+        this.setState({ quizzes: allQuizzes.quizzes, paginationModel: allQuizzes })
+
+    }
+    async getId(e) {
+        const id = e.target.dataset.id;       
+        await this.getEvents(Number(id))
+    }
     async componentDidMount() {
-        var allQuizzes = await quiz.GetAll(this.state.id);
-        this.setState({ quizzes: allQuizzes.quizzes,paginationModel:allQuizzes })
-        console.log(allQuizzes);
+        await this.getEvents(this.props.match.params.id);
     }
     render() {
         return (
             <section>
-                <Pagination nextPage = {this.state.paginationModel.hasNextPage} prev = {this.state.paginationModel.hasPreviousPage} currPage = {this.state.paginationModel.currentPage} total = {this.state.paginationModel.totalPages}/>
+                <Pagination clickHandler={this.getId.bind(this)} nextPage={this.state.paginationModel.hasNextPage} prev={this.state.paginationModel.hasPreviousPage} currPage={this.state.paginationModel.currentPage} total={this.state.paginationModel.totalPages} />
 
                 {this.state.quizzes.map(x => (<Quiz key={x.id} id={x.id} name={x.name} taken={x.taken ? x.taken : 0} grade={x.grade} votesCount={x.votesCount} creator={x.creator ? x.creator : "Admin-GameQuiz@Gmail.com"} />))}
 
-                <Pagination nextPage = {this.state.paginationModel.hasNextPage} prev = {this.state.paginationModel.hasPreviousPage} currPage = {this.state.paginationModel.currentPage} total = {this.state.paginationModel.totalPages}/>
+                <Pagination nextPage={this.state.paginationModel.hasNextPage} prev={this.state.paginationModel.hasPreviousPage} currPage={this.state.paginationModel.currentPage} total={this.state.paginationModel.totalPages} />
 
             </section >
         )
--- client/src/shared/Pagination/Pagination.js
+++ client/src/shared/Pagination/Pagination.js
@@ -2,16 +2,34 @@ import "./Pagination.css"
 import { Link } from "react-router-dom";
 const Pagination = (props) => {
     return (
-        <div className="pagination">
-            <Link className="anchor" to={{ pathname: '/all/1' }}>First</Link>
-            {props.prev ? <><Link className="anchor sign" to={{pathname:`${props.currPage - 1}`}} >&laquo;</Link>
-                <Link className="anchor">{props.currPage - 1}</Link> </> : ''}
+        <div className="pagination" onClick={props.clickHandler}>
+            <Link data-id={1} className="anchor" to={{ pathname: '/all/1' }}>
+                First
+            </Link>
+            {props.prev ? <>
+                <Link data-id={props.currPage - 1} className="anchor sign" to={{ pathname: `/all/${props.currPage - 1}` }} >
+                    &laquo;
+                </Link>
+                <Link data-id={props.currPage - 1} className="anchor" to={{ pathname: `/all/${props.currPage - 1}` }}>
+                    {props.currPage - 1}
+                </Link> </>
+                : ''}
 
-            <Link className="anchor" >{props.currPage}</Link>
+            <Link data-id={props.currPage} className="anchor" to={{ pathname: `/all/${props.currPage}` }}>{props.currPage}</Link>
 
-            {props.nextPage ? <>  <Link to={{ pathname: `/all/${props.currPage + 1}}` }} className="anchor" >{props.currPage + 1}</Link>  <Link className="anchor sign" to={{ pathname: `/all/${props.currPage + 1}}` }}>&raquo;</Link>
-                <Link className="anchor" to={{ pathname: `/all/${props.total}` }} >Last</Link>
+            {props.nextPage ? <>
+                <Link data-id={props.currPage + 1} to={{ pathname: `/all/${props.currPage + 1}` }} className="anchor" >
+                    {props.currPage + 1}
+                </Link>
+
+                <Link data-id={props.currPage + 1} className="anchor sign" to={{ pathname: `/all/${props.currPage + 1}` }}>
+                    &raquo;
+                </Link>
+
             </> : ''}
+            <Link data-id={props.total} className="anchor" to={{ pathname: `/all/${props.total}` }} >
+                Last
+            </Link>
         </div>
 
     )
